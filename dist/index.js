5ff82a39c9bc011c45df01bd1380d89226950654
Anthony Corcutt <acorcutt@gmail.com>
2021-10-28 00:05:49 +0100

    cleanup package

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-const $e9036bc7e4b4ba73$var$defaultObjectEncoder = (obj, key, next, withRef)=>withRef(()=>{
+const $f25e7000d7cb3c8b$var$defaultObjectEncoder = (obj, key, next, withRef)=>withRef(()=>{
         return {
             $: key,
             _: Object.entries(obj).reduce((acc, [key, _obj])=>{
@@ -11,7 +11,7 @@ const $e9036bc7e4b4ba73$var$defaultObjectEncoder = (obj, key, next, withRef)=>wi
         };
     })
 ;
-const $e9036bc7e4b4ba73$var$defaultObjectDecoder = (Type)=>{
+const $f25e7000d7cb3c8b$var$defaultObjectDecoder = (Type)=>{
     const obj = new Type(); // Use detected type for reuseabilty
     return [
         (json, next)=>{
@@ -23,16 +23,16 @@ const $e9036bc7e4b4ba73$var$defaultObjectDecoder = (Type)=>{
         obj, 
     ];
 };
-function $e9036bc7e4b4ba73$export$7fc33a7b2672fa1e(key = 'Object', Type = Object) {
+function $f25e7000d7cb3c8b$export$7fc33a7b2672fa1e(key = 'Object', Type = Object) {
     return {
         [key]: [
             Type,
-            $e9036bc7e4b4ba73$var$defaultObjectEncoder,
-            $e9036bc7e4b4ba73$var$defaultObjectDecoder
+            $f25e7000d7cb3c8b$var$defaultObjectEncoder,
+            $f25e7000d7cb3c8b$var$defaultObjectDecoder
         ]
     };
 }
-function $e9036bc7e4b4ba73$export$ce49242f4678ffb8(key, Type) {
+function $f25e7000d7cb3c8b$export$ce49242f4678ffb8(key, Type) {
     return {
         [key]: [
             Type,
@@ -47,7 +47,7 @@ function $e9036bc7e4b4ba73$export$ce49242f4678ffb8(key, Type) {
         ]
     };
 }
-function $e9036bc7e4b4ba73$export$92682d8a0030b3b7(key, Type) {
+function $f25e7000d7cb3c8b$export$92682d8a0030b3b7(key, Type) {
     return {
         [key]: [
             Type,
@@ -62,7 +62,7 @@ function $e9036bc7e4b4ba73$export$92682d8a0030b3b7(key, Type) {
         ]
     };
 }
-function $e9036bc7e4b4ba73$export$701eba25a361ee0f(refs = {
+function $f25e7000d7cb3c8b$export$701eba25a361ee0f(refs = {
 }, key = 'Symbol', Type = Symbol) {
     // Swap refs key -> symbol to symbol -> key
     const symbols = Object.entries(refs).reduce((map, [_key, sym])=>{
@@ -83,14 +83,14 @@ function $e9036bc7e4b4ba73$export$701eba25a361ee0f(refs = {
     };
 }
 // Object -> JSON
-const $e9036bc7e4b4ba73$var$defaultEncoder = (obj)=>obj
+const $f25e7000d7cb3c8b$var$defaultEncoder = (obj)=>obj
 ;
 // JSON -> Object
-const $e9036bc7e4b4ba73$var$defaultDecoder = ()=>[
+const $f25e7000d7cb3c8b$var$defaultDecoder = ()=>[
         (json)=>json
     ]
 ;
-const $e9036bc7e4b4ba73$var$defaultTypes = {
+const $f25e7000d7cb3c8b$var$defaultTypes = {
     // Handle decoding Undefined objects
     Undefined: [
         class Undefined {
@@ -115,7 +115,7 @@ const $e9036bc7e4b4ba73$var$defaultTypes = {
             ]
     ],
     // Extended types
-    ...$e9036bc7e4b4ba73$export$ce49242f4678ffb8('BigInt', BigInt),
+    ...$f25e7000d7cb3c8b$export$ce49242f4678ffb8('BigInt', BigInt),
     BigInt64Array: [
         BigInt64Array,
         (obj, key, next)=>({
@@ -164,15 +164,15 @@ const $e9036bc7e4b4ba73$var$defaultTypes = {
             ]
     ],
     // Typed Arrays
-    ...$e9036bc7e4b4ba73$export$92682d8a0030b3b7('Uint8Array', Uint8Array),
-    ...$e9036bc7e4b4ba73$export$92682d8a0030b3b7('Uint8ClampedArray', Uint8ClampedArray),
-    ...$e9036bc7e4b4ba73$export$92682d8a0030b3b7('Int8Array', Int8Array),
-    ...$e9036bc7e4b4ba73$export$92682d8a0030b3b7('Uint16Array', Uint16Array),
-    ...$e9036bc7e4b4ba73$export$92682d8a0030b3b7('Int16Array', Int16Array),
-    ...$e9036bc7e4b4ba73$export$92682d8a0030b3b7('Uint32Array', Uint32Array),
-    ...$e9036bc7e4b4ba73$export$92682d8a0030b3b7('Int32Array', Int32Array),
-    ...$e9036bc7e4b4ba73$export$92682d8a0030b3b7('Float32Array', Float32Array),
-    ...$e9036bc7e4b4ba73$export$92682d8a0030b3b7('Float64Array', Float64Array),
+    ...$f25e7000d7cb3c8b$export$92682d8a0030b3b7('Uint8Array', Uint8Array),
+    ...$f25e7000d7cb3c8b$export$92682d8a0030b3b7('Uint8ClampedArray', Uint8ClampedArray),
+    ...$f25e7000d7cb3c8b$export$92682d8a0030b3b7('Int8Array', Int8Array),
+    ...$f25e7000d7cb3c8b$export$92682d8a0030b3b7('Uint16Array', Uint16Array),
+    ...$f25e7000d7cb3c8b$export$92682d8a0030b3b7('Int16Array', Int16Array),
+    ...$f25e7000d7cb3c8b$export$92682d8a0030b3b7('Uint32Array', Uint32Array),
+    ...$f25e7000d7cb3c8b$export$92682d8a0030b3b7('Int32Array', Int32Array),
+    ...$f25e7000d7cb3c8b$export$92682d8a0030b3b7('Float32Array', Float32Array),
+    ...$f25e7000d7cb3c8b$export$92682d8a0030b3b7('Float64Array', Float64Array),
     // Objects that support circular references or references to other objects
     Array: [
         Array,
@@ -233,9 +233,9 @@ const $e9036bc7e4b4ba73$var$defaultTypes = {
             ];
         }, 
     ],
-    ...$e9036bc7e4b4ba73$export$7fc33a7b2672fa1e()
+    ...$f25e7000d7cb3c8b$export$7fc33a7b2672fa1e()
 };
-function $e9036bc7e4b4ba73$var$_objectToJson(obj, types, refs, fallback) {
+function $f25e7000d7cb3c8b$var$_objectToJson(obj, types, refs, fallback) {
     // These do not have constructors
     if (obj === null) return null;
     if (obj === undefined) return {
@@ -243,8 +243,8 @@ function $e9036bc7e4b4ba73$var$_objectToJson(obj, types, refs, fallback) {
     };
     const type = types.get(obj.constructor);
     if (type) {
-        const [key, get = $e9036bc7e4b4ba73$var$defaultEncoder] = type;
-        return get(obj, key, /* next */ (_obj)=>$e9036bc7e4b4ba73$var$_objectToJson(_obj, types, refs, fallback)
+        const [key, get = $f25e7000d7cb3c8b$var$defaultEncoder] = type;
+        return get(obj, key, /* next */ (_obj)=>$f25e7000d7cb3c8b$var$_objectToJson(_obj, types, refs, fallback)
         , /* withRef*/ (fn)=>{
             // If we have a reference to this object, use it
             if (refs.includes(obj)) return {
@@ -259,7 +259,7 @@ function $e9036bc7e4b4ba73$var$_objectToJson(obj, types, refs, fallback) {
         });
     } else return fallback(obj);
 }
-function $e9036bc7e4b4ba73$var$_jsonToObject(json, types, typesMap, refs, fallback) {
+function $f25e7000d7cb3c8b$var$_jsonToObject(json, types, typesMap, refs, fallback) {
     // Types without constructor
     if (json === null) return null;
     // undefined is not valid JSON maybe should throw error
@@ -268,12 +268,12 @@ function $e9036bc7e4b4ba73$var$_jsonToObject(json, types, typesMap, refs, fallba
     if (json.constructor === Object && Number.isInteger(Number.parseInt(json.$))) return refs[Number.parseInt(json.$)];
     // Native types
     if (json.constructor === Object || json.constructor === Array || json.constructor === String || json.constructor === Number || json.constructor === Boolean) {
-        const [_, __, decoder = $e9036bc7e4b4ba73$var$defaultDecoder] = (json.constructor === Object ? types[json.$] : typesMap.get(json.constructor)) || [];
-        const [fn, ref] = decoder(json.constructor === Object ? (types[json.$] || $e9036bc7e4b4ba73$var$defaultDecoder)[0] : json.constructor);
+        const [_, __, decoder = $f25e7000d7cb3c8b$var$defaultDecoder] = (json.constructor === Object ? types[json.$] : typesMap.get(json.constructor)) || [];
+        const [fn, ref] = decoder(json.constructor === Object ? (types[json.$] || $f25e7000d7cb3c8b$var$defaultDecoder)[0] : json.constructor);
         // Push ref before recursion
         if (ref) refs.push(ref);
         // Build recuresive function handler can call
-        const next = (json)=>$e9036bc7e4b4ba73$var$_jsonToObject(json, types, typesMap, refs, fallback)
+        const next = (json)=>$f25e7000d7cb3c8b$var$_jsonToObject(json, types, typesMap, refs, fallback)
         ;
         // Call jsonToObject handler
         if (json.constructor === Object) return fn(json._, next);
@@ -282,14 +282,14 @@ function $e9036bc7e4b4ba73$var$_jsonToObject(json, types, typesMap, refs, fallba
     // TODO - Should we error instead on invalid json?
     return undefined;
 }
-function $e9036bc7e4b4ba73$export$828acff4ac78a63f(obj, types = {
+function $f25e7000d7cb3c8b$export$828acff4ac78a63f(obj, types = {
 }, fallback = (obj)=>({
         $: obj.constructor.name
     })
 ) {
     // TODO - can we validate types here if not too expensive?
-    return $e9036bc7e4b4ba73$var$_objectToJson(obj, Object.entries({
-        ...$e9036bc7e4b4ba73$var$defaultTypes,
+    return $f25e7000d7cb3c8b$var$_objectToJson(obj, Object.entries({
+        ...$f25e7000d7cb3c8b$var$defaultTypes,
         ...types
     }).reduce((map, [key, [type, get, set]])=>{
         return map.set(type, [
@@ -299,15 +299,15 @@ function $e9036bc7e4b4ba73$export$828acff4ac78a63f(obj, types = {
         ]);
     }, new Map()), [], fallback);
 }
-function $e9036bc7e4b4ba73$export$92b78acdbe066b6b(json, types = {
+function $f25e7000d7cb3c8b$export$92b78acdbe066b6b(json, types = {
 }, fallback = (json)=>json
 ) {
     // TODO - can we validate types here if not too expensive?
-    return $e9036bc7e4b4ba73$var$_jsonToObject(json, {
-        ...$e9036bc7e4b4ba73$var$defaultTypes,
+    return $f25e7000d7cb3c8b$var$_jsonToObject(json, {
+        ...$f25e7000d7cb3c8b$var$defaultTypes,
         ...types
     }, Object.entries({
-        ...$e9036bc7e4b4ba73$var$defaultTypes,
+        ...$f25e7000d7cb3c8b$var$defaultTypes,
         ...types
     }).reduce((map, [key, [type, get, set]])=>{
         return map.set(type, [
@@ -317,33 +317,33 @@ function $e9036bc7e4b4ba73$export$92b78acdbe066b6b(json, types = {
         ]);
     }, new Map()), [], fallback);
 }
-function $e9036bc7e4b4ba73$export$fac44ee5b035f737(obj, types = {
+function $f25e7000d7cb3c8b$export$fac44ee5b035f737(obj, types = {
 }) {
-    return JSON.stringify($e9036bc7e4b4ba73$export$828acff4ac78a63f(obj, types));
+    return JSON.stringify($f25e7000d7cb3c8b$export$828acff4ac78a63f(obj, types));
 }
-function $e9036bc7e4b4ba73$export$98e6a39c04603d36(str, types = {
+function $f25e7000d7cb3c8b$export$98e6a39c04603d36(str, types = {
 }) {
-    return $e9036bc7e4b4ba73$export$92b78acdbe066b6b(JSON.parse(str), types);
+    return $f25e7000d7cb3c8b$export$92b78acdbe066b6b(JSON.parse(str), types);
 }
-class $e9036bc7e4b4ba73$export$541ac9d92787cb0b {
+class $f25e7000d7cb3c8b$export$541ac9d92787cb0b {
     constructor(types = {
     }){
         this.types = types;
     }
     stringify(obj) {
-        return $e9036bc7e4b4ba73$export$fac44ee5b035f737(obj, this.types);
+        return $f25e7000d7cb3c8b$export$fac44ee5b035f737(obj, this.types);
     }
     parse(str) {
-        return $e9036bc7e4b4ba73$export$98e6a39c04603d36(str, this.types);
+        return $f25e7000d7cb3c8b$export$98e6a39c04603d36(str, this.types);
     }
     jsonify(obj) {
-        return $e9036bc7e4b4ba73$export$828acff4ac78a63f(obj, this.types);
+        return $f25e7000d7cb3c8b$export$828acff4ac78a63f(obj, this.types);
     }
     objectify(json) {
-        return $e9036bc7e4b4ba73$export$92b78acdbe066b6b(json, this.types);
+        return $f25e7000d7cb3c8b$export$92b78acdbe066b6b(json, this.types);
     }
 }
 
 
-export {$e9036bc7e4b4ba73$export$7fc33a7b2672fa1e as createObjectHandler, $e9036bc7e4b4ba73$export$ce49242f4678ffb8 as createStringifiedHandler, $e9036bc7e4b4ba73$export$92682d8a0030b3b7 as createTypedArrayHandler, $e9036bc7e4b4ba73$export$701eba25a361ee0f as createReferencesHandler, $e9036bc7e4b4ba73$export$828acff4ac78a63f as jsonify, $e9036bc7e4b4ba73$export$92b78acdbe066b6b as objectify, $e9036bc7e4b4ba73$export$fac44ee5b035f737 as stringify, $e9036bc7e4b4ba73$export$98e6a39c04603d36 as parse, $e9036bc7e4b4ba73$export$541ac9d92787cb0b as JxO};
+export {$f25e7000d7cb3c8b$export$7fc33a7b2672fa1e as createObjectHandler, $f25e7000d7cb3c8b$export$ce49242f4678ffb8 as createStringifiedHandler, $f25e7000d7cb3c8b$export$92682d8a0030b3b7 as createTypedArrayHandler, $f25e7000d7cb3c8b$export$701eba25a361ee0f as createReferencesHandler, $f25e7000d7cb3c8b$export$828acff4ac78a63f as jsonify, $f25e7000d7cb3c8b$export$92b78acdbe066b6b as objectify, $f25e7000d7cb3c8b$export$fac44ee5b035f737 as stringify, $f25e7000d7cb3c8b$export$98e6a39c04603d36 as parse, $f25e7000d7cb3c8b$export$541ac9d92787cb0b as JxO};
 //# sourceMappingURL=index.js.map
